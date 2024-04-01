414f7327dc85806c6654849620353c3eda5515c5
pyduardo <eduardocomercial2006@gmail.com>
2024-04-01 17:44:24 -0300

    Palindrome Task

---
+++ 01_palindrome.py
@@ -0,0 +1,17 @@
+def palindrome(word):
+    reversed_word = word[::-1]
+    
+    print(f'"{word}" is a palindrome!') if reversed_word.lower() == word.lower() else print(f'{word} is not a palindrome...')
+
+word = input('Type a word: ')
+word = word.replace(" ", "")
+
+try:
+    word = str(word)
+except ValueError:
+    print('Type a word.')
+
+if len(word) > 2:
+    palindrome(word)
+else:
+    print('The entered word has no more than 2 characters.')
