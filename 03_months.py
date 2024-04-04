9537fd12618ffef9fc2b8a249c027a281b2f1cb8
pyduardo <eduardocomercial2006@gmail.com>
2024-04-04 13:48:32 -0300

    Months Task

---
+++ 03_months.py
@@ -0,0 +1,24 @@
+# Faça um programa que converta um numero representando um mês (1 a 12) para o nome do mês.
+
+months_list = ['January', 'February', 'March',
+               'April', 'May', 'June',
+               'July', 'August', 'September',
+               'Octuber', 'November', 'December']
+
+number = input('Enter the number of a month: ')
+
+number = number.replace(' ', '')
+
+try:
+    number = int(number)
+except ValueError:
+    print('Invalid value.')
+else:
+    for numberi, month in enumerate(months_list):
+        if number > 12 or number < 0:
+            print(f"There are no {number} months.")    
+            break
+        else:
+            if number == numberi+1:
+                print(f'The month {number} is {month}!')
+
